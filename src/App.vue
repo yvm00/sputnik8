97f63fb9cd152b7eee8c1d42648ad84e403fd1aa
Yana Mavlenko <yuna18178@gmail.com>
2024-10-23 19:01:11 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="app">
+    <navbar/>
+  </div>
+</template>
+
+<script>
+import Navbar from "@/components/Navbar.vue"
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.app {
+  display: flex;
+  justify-content: center;
+  padding-top: 50px;
+}
+</style>
